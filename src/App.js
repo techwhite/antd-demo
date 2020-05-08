1a1f23b12ac3eaffea648c1841c20fd3be0cf358
unknown <youyucai@xiaomi.com>
2020-05-08 15:43:15 +0800

    '按需加载配置'

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import { Button} from "antd";
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Button } from "antd";
 import './App.css';
 
-const App = () => (
-    <div className="App">
-        <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-        <Button type="primary">Button</Button>
-    </div>
-);
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Button type="primary">Button</Button>
+            </div>
+        );
+    }
+}
 
 export default App;
